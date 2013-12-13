8067198b77d0948cc07b53e76db2f6637ecaf7ed
Takehiro Adachi <takehiro0740@gmail.com>
2013-12-14 01:41:42 +0900

    Disable natural scrolling

--- modules/people/manifests/take/osx_settings.pp
+++ modules/people/manifests/take/osx_settings.pp
@@ -6,4 +6,12 @@ class people::take::osx_settings {
     type   => 'int',
     value  => 1,
   }
+
+  boxen::osx_defaults { 'Disable natural (touchscreen-style) scrolling':
+    user   => $::luser,
+    domain => 'NSGlobalDomain',
+    key    => 'com.apple.swipescrolldirection',
+    type   => 'bool',
+    value  => false,
+  }
 }
